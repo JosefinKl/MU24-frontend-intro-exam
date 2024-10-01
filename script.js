ba47040f6f45c4f3802a673620ae6c5990770352
josefinkl <josefin.klangefjall@gmail.com>
2024-10-01 17:33:50 +0200

    Finished G level

--- script.js
+++ script.js
@@ -3,10 +3,11 @@ console.log("HEllo world")
 const addButton = document.querySelector("#addItem")
 const addItemField = document.querySelector("#addItemField")
 const toDo = document.querySelector("#toDo")
-const save = document.querySelector("#save")
+
+
 
 let i = 0;
-const toDoActions = [];
+
 
 const addItemToHtmlWhenClicked = () => {
     i += 1;
@@ -17,28 +18,14 @@ const addItemToHtmlWhenClicked = () => {
     let labelElement = document.createElement('label')
     inputElement.setAttribute('type', 'checkbox');
     inputElement.setAttribute('id', i);
+    
     labelElement.textContent = addItemField.value;  
     newIl.appendChild(inputElement);
     newIl.appendChild(labelElement);
     toDo.appendChild(newIl);
-    toDoActions.push(toDo)
+    inputElement.addEventListener("onChanged", ()=>{console.log("klickad")})
     
-    console.log(inputElement);
-    console.log(toDoActions)
-    // Spara data i local storage
-    localStorage.setItem(index, toDo.innerHTML);
-
-    
-    // Hämta data från local storage
-    let test = localStorage.getItem(index);
         
-        
-}
-
-const addItemToStorage = () => {
-    
-    console.log("SAVE")
-    console.log(toDoActions[0]);
 }
 
 // fun AddItemToHtml (string text, bool isChecked)
@@ -58,7 +45,7 @@ const addItemToStorage = () => {
     // call AddItemToHtml
 
 addButton.addEventListener("click", addItemToHtmlWhenClicked);
-save.addEventListener("click", addItemToStorage);
+
 
 
     
